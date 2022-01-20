6cc4853dd11f4bd8a32ed67ef2c25a3bfe90229d
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2022-01-20 13:11:34 +0200

    add cloud-storage

--- service/file-storage/index.js
+++ service/file-storage/index.js
@@ -1,5 +1,5 @@
 import UploadFileService from './storage'
 import LocalFileStorage from './local-storage'
-// import CloudFileStorage from './cloud-storage'
+import CloudFileStorage from './cloud-storage'
 
-export { UploadFileService, LocalFileStorage }
+export { UploadFileService, CloudFileStorage, LocalFileStorage }
