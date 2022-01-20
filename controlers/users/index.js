77497f72894ac417f3e375d3c0818b8d87ffdda8
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2022-01-20 18:48:05 +0200

    add email

--- controlers/users/index.js
+++ controlers/users/index.js
@@ -1,7 +1,18 @@
 /* eslint-disable no-unused-vars */
 import repositoryContacts from '../../repository/contacts'
+import repositoryUsers from '../../repository/users'
 import { HttpCode } from '../../lib/constants'
-import { UploadFileService, LocalFileStorage } from '../../service/file-storage'
+import {
+    UploadFileService,
+    LocalFileStorage,
+    CloudFileStorage,
+} from '../../service/file-storage'
+
+import {
+    EmailService,
+    SenderNodemailer,
+    SenderSendgrid,
+} from '../../service/email'
 
 const aggregation = async (req, res, next) => {
     const { id } = req.params
@@ -11,18 +22,82 @@ const aggregation = async (req, res, next) => {
             .status(HttpCode.OK)
             .json({ status: 'success', code: HttpCode.OK, data })
     }
-    res.status(HttpCode.NOT_FOUND).json({ status: 'error', code: HttpCode.NOT_FOUND, message: 'Not found' })
+    res
+        .status(HttpCode.NOT_FOUND)
+        .json({ status: 'error', code: HttpCode.NOT_FOUND, message: 'Not found' })
 }
 
 const uploadAvatar = async (req, res, next) => {
     const uploadService = new UploadFileService(
         LocalFileStorage,
         req.file,
-        req.user)
+        req.user,
+    )
+
     const avatarUrl = await uploadService.updateAvatar()
+
     res
         .status(HttpCode.OK)
         .json({ status: 'success', code: HttpCode.OK, data: { avatarUrl } })
 }
 
-export { aggregation, uploadAvatar }
+const verifyUser = async (req, res, next) => {
+    const verifyToken = req.params.token
+    const userFromToken = await repositoryUsers.findByVerifyToken(verifyToken)
+    console.log(
+        '🚀 ~ file: index.js ~ line 47 ~ verifyUser ~ userFromToken',
+        userFromToken,
+    )
+
+    if (userFromToken) {
+        await repositoryUsers.updateVerify(userFromToken.id, true)
+        return res.status(HttpCode.OK).json({
+            status: 'success',
+            code: HttpCode.OK,
+            data: { message: 'Success' },
+        })
+    }
+    res.status(HttpCode.BAD_REQUEST).json({
+        status: 'success',
+        code: HttpCode.BAD_REQUEST,
+        data: { message: 'Invalid token' },
+    })
+}
+
+const repeatEmailForVerifyUser = async (req, res, next) => {
+    const { email } = req.body
+    const user = await repositoryUsers.findByEmail(email)
+    if (user) {
+        const { email, name, verifyTokenEmail } = user
+        const emailService = new EmailService(
+            process.env.NODE_ENV,
+            new SenderNodemailer(),
+        )
+
+        const isSend = await emailService.sendVerifyEmail(
+            email,
+            name,
+            verifyTokenEmail,
+        )
+        if (isSend) {
+            return res.status(HttpCode.OK).json({
+                status: 'success',
+                code: HttpCode.OK,
+                data: { message: 'Success' },
+            })
+        }
+        return res.status(HttpCode.UE).json({
+            status: 'error',
+            code: HttpCode.UE,
+            data: { message: 'Unprocessable Entity' },
+        })
+    }
+
+    res.status(HttpCode.NOT_FOUND).json({
+        status: 'error',
+        code: HttpCode.NOT_FOUND,
+        data: { message: 'User with email not found' },
+    })
+}
+
+export { aggregation, uploadAvatar, verifyUser, repeatEmailForVerifyUser }
--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 import pkg from 'mongoose'
+import { randomUUID } from 'crypto'
 import bcrypt from 'bcryptjs'
 import gravatar from 'gravatar'
 import { Role } from '../lib/constants'
@@ -43,6 +44,11 @@ const userSchema = new Schema({
     idAvatarCloud: {
         type: String,
         default: null,
+    },
+    isVerify: { type: Boolean, default: false },
+    verifyTokenEmail: {
+        type: String,
+        default: randomUUID(),
     },
 },
     {
